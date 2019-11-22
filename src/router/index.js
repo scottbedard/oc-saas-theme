db87af96048872be031717ab7cf1f80b6b604799
Scott Bedard <scottbedard90@gmail.com>
2019-11-21 22:53:49 -0700

    test setting page titles

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,24 @@ Vue.use(VueRouter);
 const routes = [
     {
         component: () => import(/* webpackChunkName: 'home' */ '@/pages/home/Home.vue'),
+        meta: {
+            title: 'Home',
+        },
         name: 'home',
         path: '/',
     },
     {
         component: () => import(/* webpackChunkName: 'about' */ '@/pages/about/About.vue'),
+        meta: {
+            title: 'About us',
+        },
         name: 'about',
         path: '/about',
     },
     {
         component: () => import(/* webpackChunkName: '404' */ '@/pages/404/404.vue'),
         meta: {
+            title: 'Page not found',
             statusCode: 404,
         },
         name: '404',
@@ -32,6 +39,10 @@ const router = new VueRouter({
 const meta = document.querySelector('meta[name="prerender-status-code"]');
 
 router.afterEach((to) => {
+    // set the page title
+    document.title = to.meta.title || 'oc-saas-theme';
+
+    // set the status code for prerendering
     const statusCode = String(to.meta.statusCode || 200);
 
     if (meta && meta.getAttribute('content') !== statusCode) {
