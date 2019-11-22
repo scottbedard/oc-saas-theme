8b32c2f4b3156cb157c76305da1cc5b5d498c720
Scott Bedard <scottbedard90@gmail.com>
2019-11-21 20:54:11 -0700

    remove unused deps

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,4 @@
 /* eslint-disable */
-const CopyPlugin = require('copy-webpack-plugin');
-const htmlMinifier = require('html-minifier');
-const path = require('path');
-
-const resolve = (...args) => path.resolve(__dirname, ...args);
-
 module.exports = {
-    configureWebpack() {
-        return {
-            plugins: [
-                new CopyPlugin([
-                    {
-                        from: resolve('./src/foo.htm'),
-                        to: resolve('./pages/foo.htm'),
-                        transform(content) {
-                            return htmlMinifier.minify(content.toString());
-                        },
-                    },
-                ]),
-            ],
-        };
-    },
+    // ...
 };
