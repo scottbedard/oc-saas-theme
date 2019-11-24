c102869e7c304cab85579d6746f7b54d42c8721c
Scott Bedard <scottbedard90@gmail.com>
2019-11-24 14:17:48 -0700

    scaffolding store initialization

--- src/main.js
+++ src/main.js
@@ -3,13 +3,27 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import boot from './app/boot';
+import initialize from './store/initialize';
 
 Vue.config.productionTip = false;
 
+// configure our vue constructor
 boot(Vue);
 
-new Vue({
+// set initial data from the dom
+const vuetober = document.querySelector('meta[name=vuetober]');
+
+if (vuetober) {
+    const data = JSON.parse(vuetober.getAttribute('content'));
+
+    initialize(store, data);
+}
+
+// create our application and mount it to the dom
+const app = new Vue({
     router,
     store,
     render: (h) => h(App),
 }).$mount('#app');
+
+window.app = app;
---
+++ src/store/initialize.js
@@ -0,0 +1,11 @@
+/**
+ * Set initial store data
+ *
+ * @return void
+ */
+export default function initialize(store, data) {
+    // user
+    if (data.user) {
+        console.log('user!', data.user);
+    }
+}
